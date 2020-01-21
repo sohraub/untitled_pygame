76ca575d6e7f81f97257016056a702900aebc261
unknown <sohraubpazuki@gmail.com>
2020-01-21 15:12:05 -0500

    added player subclasses for players and enemies

--- game_elements/character.py
+++ game_elements/character.py
@@ -2,10 +2,10 @@ from utility_functions import get_manhattan_distance
 
 
 class Character:
-    def __init__(self, name, attributes=None, buffs=None, debuffs=None, move_speed=1, in_combat=False):
+    def __init__(self, name, x=0, y=0, attributes=None, buffs=None, debuffs=None, move_speed=1, in_combat=False):
         self.name = name
-        self.x = 0
-        self.y = 0
+        self.x = x
+        self.y = y
         self.attributes = attributes if attributes is not None else {'HP':5, 'MP':0}
         self.status = {
             'buffs': buffs if buffs is not None else list(),
