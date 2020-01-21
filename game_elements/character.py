cadaa28a5103d2d81b2edfb7a24e848a6d86a66e
unknown <sohraubpazuki@gmail.com>
2020-01-21 14:48:28 -0500

    added method for character class to move

--- game_elements/character.py
+++ game_elements/character.py
@@ -1,5 +1,8 @@
+from utility_functions import get_manhattan_distance
+
+
 class Character:
-    def __init__(self, name, attributes=None, buffs=None, debuffs=None, move_speed=1):
+    def __init__(self, name, attributes=None, buffs=None, debuffs=None, move_speed=1, in_combat=False):
         self.name = name
         self.x = 0
         self.y = 0
@@ -9,3 +12,11 @@ class Character:
             'debuffs': debuffs if debuffs is not None else list()
         }
         self.move_speed = move_speed
+        self.in_combat = in_combat
+
+    def move_to(self, destination):
+        if self.in_combat:
+            if get_manhattan_distance((self.x, self.y), destination) > self.move_speed:
+                return False
+        self.x = destination[0]
+        self.y = destination[1]
